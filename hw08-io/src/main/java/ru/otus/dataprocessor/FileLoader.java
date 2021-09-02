e81c79dfbf0339e34f47bbb3b33e1ed130fd4c0d
a.panfilov <mrpanfilov@gmail.com>
2021-09-02 10:07:51 +0300

    Edit hw08-io заготовка

--- hw08-io/src/main/java/ru/otus/dataprocessor/FileLoader.java
+++ hw08-io/src/main/java/ru/otus/dataprocessor/FileLoader.java
@@ -1,16 +1,31 @@
 package ru.otus.dataprocessor;
 
 import ru.otus.model.Measurement;
+
+import java.io.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class FileLoader implements Loader {
+    String fileName;
 
     public FileLoader(String fileName) {
+        this.fileName = fileName;
     }
 
     @Override
     public List<Measurement> load() {
         //читает файл, парсит и возвращает результат
+
+//        List<Measurement> result = new ArrayList<>();
+        Object result;
+
+        try (var objectInputStream = new ObjectInputStream(new FileInputStream(this.fileName))) {
+            result = objectInputStream.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
         return null;
     }
 }
