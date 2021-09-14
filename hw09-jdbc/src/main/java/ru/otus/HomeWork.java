4a13c3907531a68655fcee68e6c6dfb6304b3a89
a.panfilov <mrpanfilov@gmail.com>
2021-09-14 15:20:35 +0300

    Edit hw09 подготовка

--- hw09-jdbc/src/main/java/ru/otus/HomeWork.java
+++ hw09-jdbc/src/main/java/ru/otus/HomeWork.java
@@ -22,54 +22,55 @@ public class HomeWork {
     private static final String USER = "usr";
     private static final String PASSWORD = "pwd";
 
-    private static final String clientName = "hw09-jdbc.src.main.java.ru.otus.crm.model.Client";
+    private static final String clientName = "ru.otus.crm.model.Client";
 
     private static final Logger log = LoggerFactory.getLogger(HomeWork.class);
 
     public static void main(String[] args) throws ClassNotFoundException {
 // Общая часть
-        var dataSource = new DriverManagerDataSource(URL, USER, PASSWORD);
-        flywayMigrations(dataSource);
-        var transactionRunner = new TransactionRunnerJdbc(dataSource);
-        var dbExecutor = new DbExecutorImpl();
+//        var dataSource = new DriverManagerDataSource(URL, USER, PASSWORD);
+//        flywayMigrations(dataSource);
+//        var transactionRunner = new TransactionRunnerJdbc(dataSource);
+//        var dbExecutor = new DbExecutorImpl();
 
 // Работа с клиентом
         EntityClassMetaData entityClassMetaDataClient = new EntityClassMetaDataImpl(clientName);
         EntitySQLMetaData entitySQLMetaDataClient = null; //= new EntitySQLMetaDataImpl();
-        var dataTemplateClient = new DataTemplateJdbc<Client>(dbExecutor, entitySQLMetaDataClient); //реализация DataTemplate, универсальная
+//        var dataTemplateClient = new DataTemplateJdbc<Client>(dbExecutor, entitySQLMetaDataClient); //реализация DataTemplate, универсальная
 
 // Код дальше должен остаться
-        var dbServiceClient = new DbServiceClientImpl(transactionRunner, dataTemplateClient);
-        dbServiceClient.saveClient(new Client("dbServiceFirst"));
-
-        var clientSecond = dbServiceClient.saveClient(new Client("dbServiceSecond"));
-        var clientSecondSelected = dbServiceClient.getClient(clientSecond.getId())
-                .orElseThrow(() -> new RuntimeException("Client not found, id:" + clientSecond.getId()));
-        log.info("clientSecondSelected:{}", clientSecondSelected);
-
-// Сделайте тоже самое с классом Manager (для него надо сделать свою таблицу)
-
-        EntityClassMetaData entityClassMetaDataManager; // = new EntityClassMetaDataImpl();
-        EntitySQLMetaData entitySQLMetaDataManager = null; //= new EntitySQLMetaDataImpl();
-        var dataTemplateManager = new DataTemplateJdbc<Manager>(dbExecutor, entitySQLMetaDataManager);
-
-        var dbServiceManager = new DbServiceManagerImpl(transactionRunner, dataTemplateManager);
-        dbServiceManager.saveManager(new Manager("ManagerFirst"));
-
-        var managerSecond = dbServiceManager.saveManager(new Manager("ManagerSecond"));
-        var managerSecondSelected = dbServiceManager.getManager(managerSecond.getNo())
-                .orElseThrow(() -> new RuntimeException("Manager not found, id:" + managerSecond.getNo()));
-        log.info("managerSecondSelected:{}", managerSecondSelected);
+//        var dbServiceClient = new DbServiceClientImpl(transactionRunner, dataTemplateClient);
+//        dbServiceClient.saveClient(new Client("dbServiceFirst"));
+//
+//        var clientSecond = dbServiceClient.saveClient(new Client("dbServiceSecond"));
+//        var clientSecondSelected = dbServiceClient.getClient(clientSecond.getId())
+//                .orElseThrow(() -> new RuntimeException("Client not found, id:" + clientSecond.getId()));
+//        log.info("clientSecondSelected:{}", clientSecondSelected);
+//
+//// Сделайте тоже самое с классом Manager (для него надо сделать свою таблицу)
+//
+//        EntityClassMetaData entityClassMetaDataManager; // = new EntityClassMetaDataImpl();
+//        EntitySQLMetaData entitySQLMetaDataManager = null; //= new EntitySQLMetaDataImpl();
+//        var dataTemplateManager = new DataTemplateJdbc<Manager>(dbExecutor, entitySQLMetaDataManager);
+//
+//        var dbServiceManager = new DbServiceManagerImpl(transactionRunner, dataTemplateManager);
+//        dbServiceManager.saveManager(new Manager("ManagerFirst"));
+//
+//        var managerSecond = dbServiceManager.saveManager(new Manager("ManagerSecond"));
+//        var managerSecondSelected = dbServiceManager.getManager(managerSecond.getNo())
+//                .orElseThrow(() -> new RuntimeException("Manager not found, id:" + managerSecond.getNo()));
+//        log.info("managerSecondSelected:{}", managerSecondSelected);
     }
+//
+//    private static void flywayMigrations(DataSource dataSource) {
+//        log.info("db migration started...");
+//        var flyway = Flyway.configure()
+//                .dataSource(dataSource)
+//                .locations("classpath:/db/migration")
+//                .load();
+//        flyway.migrate();
+//        log.info("db migration finished.");
+//        log.info("***");
+//    }
 
-    private static void flywayMigrations(DataSource dataSource) {
-        log.info("db migration started...");
-        var flyway = Flyway.configure()
-                .dataSource(dataSource)
-                .locations("classpath:/db/migration")
-                .load();
-        flyway.migrate();
-        log.info("db migration finished.");
-        log.info("***");
-    }
 }
--- hw09-jdbc/src/main/java/ru/otus/jdbc/mapper/EntityClassMetaDataImpl.java
+++ hw09-jdbc/src/main/java/ru/otus/jdbc/mapper/EntityClassMetaDataImpl.java
@@ -1,8 +1,12 @@
 package ru.otus.jdbc.mapper;
 
+import ru.otus.core.annotation.Id;
+
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class EntityClassMetaDataImpl implements EntityClassMetaData{
     private final Class<?> entityClazz;
@@ -13,26 +17,35 @@ public class EntityClassMetaDataImpl implements EntityClassMetaData{
 
     @Override
     public String getName() {
-        return null;
+        return entityClazz.getName();
     }
 
     @Override
     public Constructor getConstructor() {
-        return null;
+        Constructor<?> constructor = entityClazz.getConstructors()[0];
+        return constructor;
     }
 
     @Override
     public Field getIdField() {
-        return null;
+        Field resultField = null;
+        for (Field field : entityClazz.getFields()) {
+            if (field.isAnnotationPresent(Id.class)) {
+                resultField = field;
+                break;
+            }
+        }
+        return resultField;
     }
 
     @Override
     public List<Field> getAllFields() {
-        return null;
+        return List.of(entityClazz.getFields());
     }
 
     @Override
     public List<Field> getFieldsWithoutId() {
-        return null;
+        return (List<Field>) Arrays.stream(entityClazz.getFields()).
+                filter(field -> !field.isAnnotationPresent(Id.class));
     }
 }
